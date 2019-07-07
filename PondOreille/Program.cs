2144945b5b0516866b42c3f548908d2b4e03bbe4
JoshuaDouce <Joshuadouce@gmail.com>
2019-07-07 18:46:43 +0100

    Add project files.

---
+++ PondOreille/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PondOreille
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
