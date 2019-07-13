ece8ffcdf98c46bed372e385d9a570d040018fd4
JoshuaDouce <Joshuadouce@gmail.com>
2019-07-13 12:13:44 +0100

    Where Am I?

---
+++ WhereAmI/MapImage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereAmI
+{
+    class MapImage
+    {
+        public static void Show(GeoCoordinate location)
+        {
+            string filename = $"{location.Latitude:##.000},{location.Longitude:##.000},{location.HorizontalAccuracy:####}m.jpg";
+
+            DownloadMapImage(BuildURI(location), filename);
+
+            OpenWithDefaultApp(filename);
+        }
+
+        private static void DownloadMapImage(Uri target, string filename)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(target, filename);
+            }
+        }
+
+        private static Uri BuildURI(GeoCoordinate location)
+        {
+            var appId = "{Your app Id}";
+            var appCode = "{your app Code}";
+
+            var hereApiDns = "image.maps.cit.api.here.com";
+            var hereApiUrl = $"https://{hereApiDns}/mia/1.6/mapview";
+            var hereApiSecrets = $"&app_id={appId}&app_code={appCode}";
+
+            var latlon = $"&lat={location.Latitude}&lon={location.Longitude}";
+
+            return new Uri(hereApiUrl + $"?u={location.HorizontalAccuracy}{hereApiSecrets}{latlon}");
+        }
+
+        private static void OpenWithDefaultApp(string filename)
+        {
+            var si = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C start {filename}",
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            Process.Start(si);
+        }
+    }
+}
---
+++ WhereAmI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereAmI
+{
+    class Program
+    {
+        private static GeoCoordinateWatcher Watcher;
+        static void Main(string[] args)
+        {
+            Watcher = new GeoCoordinateWatcher();
+            Watcher.StatusChanged += Watcher_StatusChanged;
+            Watcher.PositionChanged += Watcher_PositionChanged;
+            Watcher.MovementThreshold = 100;
+            Watcher.Start();
+            Console.ReadKey();
+
+        }
+
+        private static void Watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            Watcher.Stop();
+
+            Console.WriteLine($"Position Changed: {e.Position.Location}");
+
+            MapImage.Show(e.Position.Location);
+        }
+
+        private static void Watcher_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            Console.WriteLine($"Status Changed: {e.Status}");
+        }
+    }
+}
