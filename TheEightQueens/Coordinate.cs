92bee7bc4ad3c193551ec0ec1bdd834edd36874f
JoshuaDouce <Joshuadouce@gmail.com>
2019-07-15 21:16:55 +0100

    8Queens Started

---
+++ TheEightQueens/Coordinate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheEightQueens
+{
+    class Coordinate
+    {
+        public int xPos { get; set; }
+        public int yPos { get; set; }
+    }
+}
---
+++ TheEightQueens/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TheEightQueens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var board = new bool[,] {
+                { false, false, false, false, false, false, false, false },
+                { false, false, false, false, false, false, false, false }};
+            var solutionFound = true;
+
+            var queenPositions = new Coordinate[8];
+
+            //if solution not found
+            //check board for available positions
+            //if available position place queen
+            //check horizontal
+            //check vertical
+            //check diagonal
+            //if no attacks - place queen
+            //if attacks - break and try again
+
+        }
+    }
+}
